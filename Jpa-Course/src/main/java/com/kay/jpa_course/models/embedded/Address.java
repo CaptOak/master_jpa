547ebdac6f8108ddf4a7575ae2e2359e00ecfc12
CaptOak <iamkanukingsley@gmail.com>
2023-08-03 02:29:56 +0200

    Implemented Embedded entity and id

---
+++ Jpa-Course/src/main/java/com/kay/jpa_course/models/embedded/Address.java
@@ -0,0 +1,18 @@
+package com.kay.jpa_course.models.embedded;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class Address {
+
+    private String streetName;
+    private String houseNumber;
+    private String zipCode;
+}
